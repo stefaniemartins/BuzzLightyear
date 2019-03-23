cd46a748f3f0f70458fd1b217c6eef013d36d7fe
stefaniemartins <stefanie.m@aluno.ifsc.edu.br>
2019-03-23 14:15:32 -0300

    Construtor da classe Buzz criado.

--- src/poo/Buzz.java
+++ src/poo/Buzz.java
@@ -10,4 +10,19 @@ public class Buzz
     private boolean asas;
     private String frases[] = new String[6];
     Random random = new Random();
+
+    public Buzz(boolean capacete, boolean braço, boolean laser, boolean asas)
+    {
+        this.capacete = capacete;
+        this.braço = braço;
+        this.laser = laser;
+        this.asas = asas;
+        
+        frases[0] = "To infinity... and beyond!";
+        frases[1] = "This isn't flying. This is falling with style!";
+        frases[2] = "Sheriff, this is no time to panic.";
+        frases[3] = "I've set my laser from stun to kill.";
+        frases[4] = "I'm gonna teach that boy a lesson!";
+        frases[5] = "Yes I can!";
+    }
 }
